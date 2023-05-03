7d4d5b3a40b05281a9d5936a4453ded9fe7e3565
Marianna Ordakhova <mariordakhova@gmail.com>
2023-05-03 17:52:02 +0900

    add PersonTest

--- src/main/java/ru/itsjava/collections/lists/Person.java
+++ src/main/java/ru/itsjava/collections/lists/Person.java
@@ -1,4 +1,22 @@
 package ru.itsjava.collections.lists;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class Person {
+
+    private String name;
+    private int age;
+
+    public void birthday() {
+        age++;
+    }
+
+    public boolean takeBeer() {
+        if (age > 18) return true;
+        return false;
+    }
 }
+
