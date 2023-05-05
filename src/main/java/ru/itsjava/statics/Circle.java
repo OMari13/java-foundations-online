f0a022e310424f6a0606f93cd0ea16cdb15c280d
Marianna Ordakhova <mariordakhova@gmail.com>
2023-05-05 21:16:57 +0900

    add Circle

--- src/main/java/ru/itsjava/statics/Circle.java
+++ src/main/java/ru/itsjava/statics/Circle.java
@@ -1,4 +1,11 @@
 package ru.itsjava.statics;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
 public class Circle {
+
+    public final static double CIRCLE_CIRCLE = 20.0;
+
 }
