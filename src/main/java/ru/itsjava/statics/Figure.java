b9a9e2facc90bae638b855a9095ae77c10d79201
Marianna Ordakhova <mariordakhova@gmail.com>
2023-05-05 21:16:30 +0900

    add Circle

--- src/main/java/ru/itsjava/statics/Figure.java
+++ src/main/java/ru/itsjava/statics/Figure.java
@@ -1,4 +1,9 @@
 package ru.itsjava.statics;
 
 public class Figure {
+    public static void main(String[] args) {
+
+
+        System.out.println("Circle.CIRCLE_CIRCLE = " + Circle.CIRCLE_CIRCLE);
+    }
 }
