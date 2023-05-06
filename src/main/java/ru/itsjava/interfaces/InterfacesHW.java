9588dc2de4919946457b6518a4b7f88489f0f298
Marianna Ordakhova <mariordakhova@gmail.com>
2023-05-06 17:11:50 +0900

    add InterfacesHW

---
+++ src/main/java/ru/itsjava/interfaces/InterfacesHW.java
@@ -0,0 +1,22 @@
+package ru.itsjava.interfaces;
+
+public class InterfacesHW {
+    public static void main(String[] args) {
+
+        Walkable man = new Man();
+        man.walk();
+
+        Runnable cat = new Cat();
+        cat.run();
+        cat.maxRunDistance();
+
+        Swimable fish = new Fish();
+        fish.swim();
+        fish.maxSwimSpeed();
+
+        Flyable bird = new Bird();
+        bird.fly();
+        bird.flyMaxDistance();
+
+    }
+}
