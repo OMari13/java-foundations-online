7b769be0d5f6d255469341bc950c1e8a1372a510
Marianna Ordakhova <mariordakhova@gmail.com>
2023-05-08 14:03:01 +0900

    add Main

--- src/main/java/ru/itsjava/interfaces/comparable/Main.java
+++ src/main/java/ru/itsjava/interfaces/comparable/Main.java
@@ -10,12 +10,25 @@ public class Main {
         Person son = new Person("Son", "Ivanov", 18);
         Person daughter = new Person("Daughter", "Ivanova", 15);
 
+
+        System.out.println();
+
+
         System.out.println("mother.compareTo(father) = " + mother.compareTo(father));
-        System.out.println("father.compareTo(son) = " + father.compareTo(son));
         System.out.println("mother.compareTo(son) = " + mother.compareTo(son));
         System.out.println("mother.compareTo(daughter) = " + mother.compareTo(daughter));
+        System.out.println("father.compareTo(mother) = " + father.compareTo(mother));
+        System.out.println("father.compareTo(son) = " + father.compareTo(son));
         System.out.println("father.compareTo(daughter) = " + father.compareTo(daughter));
+        System.out.println("son.compareTo(mother) = " + son.compareTo(mother));
+        System.out.println("son.compareTo(father) = " + son.compareTo(father));
         System.out.println("son.compareTo(daughter) = " + son.compareTo(daughter));
+        System.out.println("daughter.compareTo(mother) = " + daughter.compareTo(mother));
+        System.out.println("daughter.compareTo(father) = " + daughter.compareTo(father));
+        System.out.println("daughter.compareTo(son) = " + daughter.compareTo(son));
+
+
+        System.out.println();
 
 
         Person[] people = {mother, father, son, daughter};
@@ -23,10 +36,18 @@ public class Main {
         Arrays.sort(people);
         System.out.println("Arrays.toString(people) = " + Arrays.toString(people));
 
+
+        System.out.println();
+
+
         Person mother1 = (Person) mother.clone();
         Person father1 = (Person) father.clone();
         Person son1 = (Person) son.clone();
         Person daughter1 = (Person) daughter.clone();
+
+
+        System.out.println();
+
 
         System.out.println("mother = " + mother);
         System.out.println("mother1 = " + mother1);
