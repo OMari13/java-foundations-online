d3b274a38d4170263b7d366548b4c7dec894c523
Marianna Ordakhova <mariordakhova@gmail.com>
2023-05-21 23:17:54 +0900

    class Person for the homework ArrayList

---
+++ src/main/java/ru/itsjava/collections/lists/Book.java
@@ -0,0 +1,15 @@
+package ru.itsjava.collections.lists;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class Book {
+
+    private String name;
+
+    private String author;
+    private int pages;
+
+}
