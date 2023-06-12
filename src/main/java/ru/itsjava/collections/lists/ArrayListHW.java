0b0b153d2a28c46a18a57375bd2b280aa610a028
Marianna Ordakhova <mariordakhova@gmail.com>
2023-06-12 21:19:06 +0900

    Домашнее задание ArrayList

--- src/main/java/ru/itsjava/collections/lists/ArrayListHW.java
+++ src/main/java/ru/itsjava/collections/lists/ArrayListHW.java
@@ -23,7 +23,10 @@ public class ArrayListHW {
         Book choice = new Book("Выбор", "Эгер", 400);
         Book pushkin = new Book("Капитанская дочка", "Пушкин", 700);
         Book turgenev = new Book("Отцы и дети", "Тургенев", 600);
-
+        Book sierra = new Book("Изучаем Java", "Сьерра", 600);
+        Book ekkel = new Book("Философия Java", "Тургенев", 400);
+        Book shieldt = new Book("Java полное руководство", "Тургенев", 800);
+        Book sasha = new Book("Java", "Саша", 550);
 
         //1. Задачи на вставку элемента:
         //а) Добавить подряд 5 элементов.
@@ -33,11 +36,14 @@ public class ArrayListHW {
         bookList.add(greenMile);
         bookList.add(oneInBerlin);
         bookList.add(choice);
+        bookList.add(sierra);
+        bookList.add(ekkel);
+        bookList.add(shieldt);
+        bookList.add(sasha);
 
 
         //б) Добавить шестой элемент в третию позицию. Добавить седьмой элемент в первую позицию.
-
-        bookList.add(1, pushkin);
+        bookList.add(2, pushkin);
         bookList.add(0, turgenev);
 
         System.out.println("Полный список: " + bookList);
@@ -70,7 +76,7 @@ public class ArrayListHW {
         //а) Удалить элемент по значению (книге)
         //б) Удалить элемент по индексу
 
-//        bookList.remove(5);
+        //bookList.remove(5);
         bookList.remove(oneInBerlin);
         System.out.println("Лист после удаления одного элемента: " + bookList);
 
@@ -81,10 +87,85 @@ public class ArrayListHW {
 
 
         //5. Вывод списка:
-
+        System.out.println("Вывод списка:");
         for (Book allBooks : bookList) {
             System.out.print(allBooks + "; ");
+        }
+        System.out.println();
 
+
+        //6. Отфильтровать список вернуть записи по некоторому условию:
+        // а) Условие на индекс: Индекс делится на 3
+        System.out.println("Индекс делится на 3");
+        for (int i = 0; i < bookList.size(); i = i + 3) {
+            System.out.println(bookList.get(i));
+        }
+        System.out.println();
+
+
+        //б) Условие на значение: Вернуть количество книг, которые равны "Java"
+//        int num = 0;
+//        num = num + 1;
+//       System.out.println(num);
+        System.out.println("Вернуть количество книг, которые равны Java: ");
+        //индекс  0,       1
+        //список: философия java, java для начинающих
+        int bookCount = 0;
+        for (int i = 0; i < bookList.size(); i++) {
+            if (bookList.get(i).getName().contains("Java")) {
+                bookCount++;
+            }
+        }
+        System.out.println(bookCount);
+
+
+        System.out.println();
+
+        //7. Пропустить несколько первых элементов
+        //а) Пропустить первые 3 книги в списке.
+        System.out.println("Пропустить первые 3 книги в списке");
+        for (int i = 3; i < bookList.size(); i++) {
+            System.out.println(bookList.get(i));
+        }
+
+        System.out.println();
+
+        //6 + 7: Пропускаем элементы, которые удовлетворяют некоторому условию
+        //а) Пропустить первые 2 книги, которые равные "Java"
+        System.out.println("Пропустить первые 2 книги, которые равные Java");
+        int bookCount1 = 0;
+        for (int i = 0; i < bookList.size(); i++) {
+            if (bookList.get(i).getName().contains("Java") && bookCount1 < 2) {
+                bookCount1++;
+            } else {
+                System.out.println(bookList.get(i));
+            }
+        }
+        System.out.println();
+
+        //8. Вернуть первый подходящий элемент:
+        //а) Возвращаем первую книгу, длина автора которого делится на 3.
+        System.out.println("Возвращаем первую книгу, длина автора которого делится на 3");
+        int count = 0;
+        for (int i = 0; i < bookList.size(); i++) {
+            if (bookList.get(i).getAuthor().length() % 3 == 0 && count == 0) {
+                count++;
+                System.out.println(bookList.get(i));
+            }
+        }
+        System.out.println();
+
+        //9. Возвращаем все элементы удовлетворяющие условию:
+        //а) Возвращаем все книги, длина автора которых делится на 3
+        System.out.println("Возвращаем все книги, длина автора которых делится на 3");
+        for (int i = 0; i < bookList.size(); i++) {
+            if (bookList.get(i).getAuthor().length() % 3 == 0) {
+                System.out.println(bookList.get(i));
+            }
         }
     }
 }
+
+
+
+
