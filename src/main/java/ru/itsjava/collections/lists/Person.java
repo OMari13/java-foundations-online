f3daf589c890167464a5ebc806ec9facbbccc5b7
Marianna Ordakhova <mariordakhova@gmail.com>
2023-06-18 00:29:27 +0900

    Класс Person к 10 заданию по АррэйЛисту

--- src/main/java/ru/itsjava/collections/lists/Person.java
+++ src/main/java/ru/itsjava/collections/lists/Person.java
@@ -3,20 +3,11 @@ package ru.itsjava.collections.lists;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
-@Data
 @AllArgsConstructor
+@Data
 public class Person {
 
     private String name;
     private int age;
-
-    public void birthday() {
-        age++;
-    }
-
-    public boolean takeBeer() {
-        if (age > 18) return true;
-        return false;
-    }
+    private boolean isMale;
 }
-
